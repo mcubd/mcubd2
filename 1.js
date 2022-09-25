cc50cf0e30e09179d4fbce2d4d61365ed5a495f3
zayn <mdrahul6967@gmail.com>
2022-09-25 20:49:34 +0600

    hjh

--- 1.js
+++ 1.js
@@ -485,7 +485,9 @@ function imgdiv(bname, bimg,videolink,size) {
 
     chidiv.classList.add('chi');
     iiidiv.classList.add('iii');
-    image.src = '../logoimg/'+bimg+'.jpg'
+    // image.src = '../logoimg/'+bimg+'.jpg'
+    image.src = 'https://share.bdff.workers.dev/0:/logoimg/'+bimg+'.jpg'
+
 
     iiidiv.append(image)
     chidiv.append(iiidiv, p)
