ebcf6a0af5ffc942ead7cd6b5488792e5a69900b
zayn <mdrahul6967@gmail.com>
2022-10-06 18:10:15 +0600

    hjh

--- others/1.js
+++ others/1.js
@@ -1873,6 +1873,10 @@ setTimeout(() => {
 
 
 
+    seris('Doctors {Season 1} 1080p [150 MB]','doc',['https://share.bdff.workers.dev/0:/OTHERS/doc/Doctors.S01E01.mkv','https://share.bdff.workers.dev/0:/OTHERS/doc/Doctors.S01E02..mkv'],'150MB')
+
+
+
 }, 1);
 
 
