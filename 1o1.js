c6283c51a7e87fc6867b8262eddb63180134e316
zayn <mdrahul6967@gmail.com>
2022-10-14 20:11:49 +0600

    hjh

--- 1o1.js
+++ 1o1.js
@@ -63,7 +63,7 @@ order.setAttribute('onclick', 'order()')
 button.innerText = 'Contact me?/Movie request'
 h4.innerText = 'Problem in online watching? Download the video and watch it '
 button1.innerText = 'Marvel'
-button2.innerText = 'MCU-Seris'
+button2.innerText = 'Marvel-Seris'
 button3.innerText = 'M. v2'
 button4.innerText = 'Others'
 order.innerText = ' Orderly marvel-mcu movies'
