74f7d5d15c6ef9498dc4c90724909c836c71e981
zayn <mdrahul6967@gmail.com>
2022-11-17 21:02:55 +0600

    hjh

--- 1o1.js
+++ 1o1.js
@@ -61,7 +61,7 @@ order.setAttribute('onclick', 'order()')
 
 
 button.innerText = 'Contact me?/Movie request'
-h4.innerText = 'Problem in online watching? Download the video and watch it '
+h4.innerText = ''
 button1.innerText = 'Marvel'
 button2.innerText = 'Marvel-Seris'
 button3.innerText = 'fox & sony'
