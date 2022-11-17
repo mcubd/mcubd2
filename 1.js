5419918e4f0f3d4f6bedfb2eac0957b8311d523b
zayn <mdrahul6967@gmail.com>
2022-11-17 21:53:13 +0600

    hjh

--- 1.js
+++ 1.js
@@ -533,8 +533,8 @@ function imgdiv(bname, bimg,videolink,size) {
         document.getElementById('intro').innerHTML=''
         document.getElementById('h4').style.display = ''
         document.body.style.cssText = 'background-image:none;background-color: #0F0F0F;color: rgb(255, 255, 255);margin: 0px;padding: 0px;margin-left: 1.2%;margin-right: 1.2%;'
-        document.getElementById('order').style.cssText='padding: 8px   calc(100vw*.06) 15px calc(100vw*.06); background-color:rgba(0, 0, 0, 0.1);color: rgba(255, 255, 255, 0.4);margin-bottom: 45px; outline: none !important;border: none !important;box-shadow: none !important;'
-
+        // document.getElementById('order').style.cssText='padding: 8px   calc(100vw*.06) 15px calc(100vw*.06); background-color:rgba(0, 0, 0, 0.1);color: rgba(255, 255, 255, 0.4);margin-bottom: 45px; outline: none !important;border: none !important;box-shadow: none !important;'
+        document.getElementById('order').innerHTML=''
 
 
         var con = document.getElementById('cont')
