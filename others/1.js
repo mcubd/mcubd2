9820f8f8b619b23305b939895a8f0247bd67482d
zayn <mdrahul6967@gmail.com>
2023-01-28 20:51:49 +0600

    hjhd

--- others/1.js
+++ others/1.js
@@ -2757,7 +2757,7 @@ setTimeout(() => {
 
     seris('Jab We Met   [1.3GB]','jab',['https://zaynr2h.bdff.workers.dev/0:/mcubd/Others/Jab-We-Met.mp4'],'1.3GB')
     
-        seris('Cheeni (চিনি) [768MB]','chini',['https://root.bdff.workers.dev/0:/mcubd/others/chini.mp4'],'768MB')
+        seris('Cheeni  [768MB]','chini',['https://root.bdff.workers.dev/0:/mcubd/others/chini.mp4'],'768MB')
 
     seris('Your Name  (2016) [903MB]','name',['https://root.bdff.workers.dev/0:/mcubd/others/your-name.mkv'],'903MB')
 
