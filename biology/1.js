0eb11d29c0b50464d5b9a647ba8dba233df536ed
buetx <106980362+buetx@users.noreply.github.com>
2023-09-18 21:35:03 +0600

    Update 1.js

--- biology/1.js
+++ biology/1.js
@@ -13,7 +13,7 @@ fetch('./biology/'+'1'+'.json') // Replace 'data.json' with the path to your JSO
   .then(data => {
     // 'data' now contains the parsed JSON data
     var dataa=data// You can access and use the data here
-  console.log(typeof dataa)
+  console.log( dataa.1)
 
     for(let i = 0; i < dataa[0].length; i++){
  // alert(arr[i]);
