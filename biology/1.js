751e821c6328681bb11eff7e746f8e99f1f29858
buetx <106980362+buetx@users.noreply.github.com>
2023-09-18 19:47:00 +0600

    Update 1.js

--- biology/1.js
+++ biology/1.js
@@ -4,9 +4,9 @@ document.body.innerHTML = `
   <p class="main1">
   <span class="word wisteria">tasty.</span>
     <span class="word belize">কসাই .</span>
-    <span class="word pomegranate">fancsjsjdjdkdjxkxjxjxxky.</span>
+    <span class="word pomegranate">hello there whatsupp kiobostavai.</span>
     <span class="word green">beautiful.</span>
-    <span class="word midnight">cheap easy fast affordable yummy gast sust cool buoijo jamsoi kdghpouy dkdkd dkdkdk smdkdk dndmdkd dkdkdk dkdkdk.</span>
+    <span class="word midnight">cheapset on our head.</span>
   </p>
   <p></p>
 </div>
