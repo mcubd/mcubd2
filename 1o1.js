272252854391ef8285858f23dbb7097fc7290e9b
buetx <106980362+buetx@users.noreply.github.com>
2023-10-01 18:52:20 +0600

    Update 1o1.js

--- 1o1.js
+++ 1o1.js
@@ -61,7 +61,7 @@ order.setAttribute('onclick', 'order()')
 contact.innerText = 'Movies!'
 // contact.innerText = ''
 
-h4.innerText = 'sorry eto late kore deuar jonno'
+h4.innerText = ''
 button1.innerText = 'Marvel'
 button2.innerText = 'Marvel-Seris'
 button3.innerText = 'fox & sony'
